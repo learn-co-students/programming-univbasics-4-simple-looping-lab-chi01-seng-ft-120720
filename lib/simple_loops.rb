3a4488173081b6ea825e12b86d5f3c772a053350
James Sapp <jameseisapp@gmail.com>
2020-12-04 21:28:17 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,36 +1,62 @@
 # Write your methods here
+require 'pry'
 
 def loop_message_five_times(message)
-  count = 0 
-  
-  while count < 5 do 
-    puts message
-    count += 1 
+  5.times do 
+  puts message
   end 
-end
+end 
+
+# def loop_message_five_times(message)
+#   count = 0 
+#   while count < 5 do 
+#     puts message
+#     count += 1 
+#   end 
+# end
+
 
 def loop_message_n_times(message, number)
-  count = 0 
-  
-  while count <= number do
-    puts message
-    count += 1
-  end
-end 
-
-def output_array(array)
-  counter = 0 
-   while array[counter] do
-    puts array[counter]
-    counter += 1 
+  arr = []
+  arr << message
+  arr.cycle(number) do |x|
+    puts x 
   end 
 end 
 
+# def loop_message_n_times(message, number)
+#   count = 0 
+#   while count <= number do
+#     puts message
+#     count += 1
+#   end
+# end 
+
+def output_array(array)
+  array.each do |element|
+    puts element
+  end 
+end 
+
+# def output_array(array)
+#   counter = 0 
+#   while array[counter] do
+#     puts array[counter]
+#     counter += 1 
+#   end 
+# end 
+
 def return_string_array(array)
-  count = 0
-  while count < array.length do
-    array[count] = array[count].to_s
-    count += 1
+  array.map do |element|
+    element.to_s 
   end
-  array
-end
+end 
+
+# def return_string_array(array)
+#   count = 0
+#   while count < array.length do
+#     array[count] = array[count].to_s
+#     count += 1
+#   end
+#   array
+# end
